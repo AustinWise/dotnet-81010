b87b214d9b9869861e4462fbcdce790323d653b4
Austin Wise <AustinWise@gmail.com>
2023-02-05 21:54:12 -0800

    inital

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+
+static class Program
+{
+    static void FillStack(byte value)
+    {
+        Span<byte> bytes = stackalloc byte[1024 * 16];
+        bytes.Fill(value);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.ReadLine();
+        if (args.Length != 0)
+        {
+            byte fillValue = byte.Parse(args[0], NumberStyles.HexNumber);
+            AppDomain.CurrentDomain.UnhandledException += (_,_) => FillStack(fillValue);
+        }
+        throw new Exception();
+    }
+}
